db6978ffcab6a04ff2523ce8fa1992bb79eba6a7
NOTREALEPIC <165712926+NOTREALEPIC@users.noreply.github.com>
2025-04-08 14:30:44 +0530

    Create files.py

---
+++ files.py
@@ -0,0 +1,18 @@
+files_data = {
+    "BMW_M5": {
+        "file_size": "34 MB",
+        "version": "v1.2",
+        "for": "GTA 5",
+        "last_update": "2025-04-08",
+        "license": "Free-Personal",
+        "password": "bmwpass123"
+    },
+    "Lamborghini_Aventador": {
+        "file_size": "58 MB",
+        "version": "v3.0",
+        "for": "GTA 5",
+        "last_update": "2025-03-01",
+        "license": "ASU-Commercial",
+        "password": "lambo456"
+    }
+}
