bc32f2b0b80a752da042b7d32a7071e965e9b1be
NOTREALEPIC <165712926+NOTREALEPIC@users.noreply.github.com>
2025-04-08 15:56:48 +0530

    Update files.py

--- files.py
+++ files.py
@@ -1,28 +1,28 @@
 # files.py
 
 files_data = {
-    "```BMW_M5```": {
-        "size": "```25MB```",
-        "version": "```v1.2```",
-        "for": "```GTA 5```",
-        "last_update": "```2025-04-08```",
-        "license": "```ASU-Public```",
-        "password": "```bmwpass123```"
+    "BMW_M5": {
+        "size": "25MB",
+        "version": "v1.2",
+        "for": "GTA 5",
+        "last_update": "2025-04-08",
+        "license": "ASU-Public",
+        "password": "bmwpass123"
     },
-    "```Lamborghini_Aventador```": {
-        "size": "```32MB```",
-        "version": "```v2.1```",
-        "for": "```GTA 5```",
-        "last_update": "```2025-03-21```",
-        "license": "```Creative License```",
-        "password": "```lambo2025```"
+    "Lamborghini_Aventador": {
+        "size": "32MB",
+        "version": "v2.1",
+        "for": "GTA 5",
+        "last_update": "2025-03-21",
+        "license": "Creative License",
+        "password": "lambo2025"
     },
-    "```Toyota_Supra```": {
-        "size": "```28MB```",
-        "version": "```v1.0```",
-        "for": "```GTA SA``",
-        "last_update": "```2025-01-15```",
-        "license": "```Personal Use License```",
-        "password": "```supra123```"
+    "Toyota_Supra": {
+        "size": "28MB",
+        "version": "v1.0",
+        "for": "GTA SA``",
+        "last_update": "2025-01-15",
+        "license": "Personal Use License",
+        "password": "supra123"
     },
 }
