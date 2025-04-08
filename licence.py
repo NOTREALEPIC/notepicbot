cbb6eae10df573cab9e6c909063e7a4199d92a8f
NOTREALEPIC <165712926+NOTREALEPIC@users.noreply.github.com>
2025-04-08 14:48:07 +0530

    Update licence.py

--- licence.py
+++ licence.py
@@ -1,5 +1,5 @@
 license_descriptions = {
-    "Free-Personal": "This mod is free for personal use. Do not redistribute.",
-    "ASU-Commercial": "This mod is licensed for commercial use under ASU terms.",
-    "Private": "This mod is only shared privately. Unauthorized sharing is prohibited."
+    "Free-Personal": "```This mod is free for personal use. Do not redistribute.```",
+    "ASU-Commercial": "```This mod is licensed for commercial use under ASU terms.```",
+    "Private": "```This mod is only shared privately. Unauthorized sharing is prohibited.```"
 }
