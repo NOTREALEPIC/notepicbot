f843fca335056e27994483a0de1a4ab9eac8f2be
NOTREALEPIC <165712926+NOTREALEPIC@users.noreply.github.com>
2025-04-08 20:55:20 +0530

    Update files.py

--- files.py
+++ files.py
@@ -1,28 +1,21 @@
 # files.py
 
 files_data = {
-    "BMW_M5": {
-        "size": "25MB",
-        "version": "v1.2",
+    "ZedOne_Beta_Free": {
+        "size": "17MB",
+        "version": "beta",
         "for": "GTA 5",
-        "last_update": "2025-04-08",
-        "license": "ASU-Public",
-        "password": "bmwpass123"
+        "last_update": "20-07-2024",
+        "license": "Personal Use License",
+        "password": "7612"
     },
-    "Lamborghini_Aventador": {
-        "size": "32MB",
-        "version": "v2.1",
-        "for": "GTA 5",
+    "ZedOne_Beta_Free_3D_Model": {
+        "size": "55MB",
+        "version": "beta",
+        "for": "3D model",
         "last_update": "2025-03-21",
         "license": "Creative License",
-        "password": "lambo2025"
+        "password": "7685"
     },
-    "Toyota_Supra": {
-        "size": "28MB",
-        "version": "v1.0",
-        "for": "GTA SA",
-        "last_update": "2025-01-15",
-        "license": "Personal Use License",
-        "password": "supra123"
-    },
+    
 }
