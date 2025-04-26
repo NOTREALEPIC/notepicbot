1e791064f59aabd750280cedf226f7900bea1ee1
NOTREALEPIC <165712926+NOTREALEPIC@users.noreply.github.com>
2025-04-27 00:12:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -61,14 +61,14 @@ async def pass_command(interaction: discord.Interaction, modelname: str):
     # Embed with formatted block content
     embed = Embed(title=f" Access: {modelname}", color=0x2ecc71)
 
-    embed.add_field(name="```|```FILE NAME", value=f"```{modelname}```", inline=False)
-    embed.add_field(name="```|```FILE SIZE", value=f"```{file_size}```", inline=True)
-    embed.add_field(name="```|```VERSION", value=f"```{version}```", inline=True)
-    embed.add_field(name="```|```FOR", value=f"```{for_}```", inline=True)
-    embed.add_field(name="```|```LAST UPDATE", value=f"```{last_update}```", inline=True)
-    embed.add_field(name="```|```LICENSE", value=f"```{license_type}```", inline=True)
-    embed.add_field(name="```|```LICENSE DETAILS", value=f"```{license_desc}```", inline=False)
-    embed.add_field(name="```|```PASSWORD", value=f"```{password}```", inline=False)
+    embed.add_field(name="```|``` FILE NAME", value=f"```{modelname}```", inline=False)
+    embed.add_field(name="```|``` FILE SIZE", value=f"```{file_size}```", inline=True)
+    embed.add_field(name="```|``` VERSION", value=f"```{version}```", inline=True)
+    embed.add_field(name="```|``` FOR", value=f"```{for_}```", inline=True)
+    embed.add_field(name="```|``` LAST UPDATE", value=f"```{last_update}```", inline=True)
+    embed.add_field(name="```|``` LICENSE", value=f"```{license_type}```", inline=True)
+    embed.add_field(name="```|``` LICENSE DETAILS", value=f"```{license_desc}```", inline=False)
+    embed.add_field(name="```|``` PASSWORD", value=f"```{password}```", inline=False)
 
     await interaction.response.send_message(embed=embed, ephemeral=True)
 
