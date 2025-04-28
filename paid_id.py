30f3106792ed70811e5350bd2ec8efb01e62dc4a
NOTREALEPIC <165712926+NOTREALEPIC@users.noreply.github.com>
2025-04-28 20:52:46 +0530

    Update and rename paid_id.txt to paid_id.py

--- paid_id.py
+++ paid_id.py
@@ -1,16 +1,16 @@
 
 paid_id_data = {
     "epic0000": {
-        "Discord id": "42424242424",
-        "File Name": "beta",
-        "For": "GTA 5",
+        "Discord_id": "42424242424",
+        "File_Name": "beta",
+        "For_": "GTA 5",
         "Date": "20-07-2024",
         "Via": "Personal Use License"
     },
     "epic0001": {
-        "Discord id": "42424242424",
-        "File Name": "beta",
-        "For": "GTA 5",
+        "Discord_id": "42424242424",
+        "File_Name": "beta",
+        "For_": "GTA 5",
         "Date": "20-07-2024",
         "Via": "Personal Use License"
     },
