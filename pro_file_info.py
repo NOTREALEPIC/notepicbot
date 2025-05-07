ffd916f144874078ed20989b82188e2c2da0bf40
NOTREALEPIC <165712926+NOTREALEPIC@users.noreply.github.com>
2025-05-07 22:42:38 +0530

    Update pro_file_info.py

--- pro_file_info.py
+++ pro_file_info.py
@@ -2,66 +2,66 @@ pro_file_info = {
     "Lightning_McQueen_Cars_3_EPIC_Edition": {
         "INFO": """  ## <a:lightning:1369441281264189601> **Lightning McQueen – Cars 3 EPIC Edition**
 
-> <a:epicapproved:1369452022004191304> *High-Quality GTA V Mod Inspired by Disney-Pixar’s Cars 3*
-> <a:epicapproved:1369452022004191304> *Movie-accurate 3D model built with premium textures and clean details*
+                > <a:epicapproved:1369452022004191304> *High-Quality GTA V Mod Inspired by Disney-Pixar’s Cars 3*
+                > <a:epicapproved:1369452022004191304> *Movie-accurate 3D model built with premium textures and clean details*
 
-### 🔍 **MOD HIGHLIGHTS**
+                ### 🔍 **MOD HIGHLIGHTS**
 
-<a:animatedboost:1234023770692390922> **4K Ultra-HD Model** – Meticulously detailed for realism
-<a:animatedboost:1234023770692390922> **Fully Working Lights** – Headlights, brake lights & indicators
-<a:animatedboost:1234023770692390922> **Movie-Accurate Design** – Faithfully recreated from *Cars 3*
-<a:animatedboost:1234023770692390922> **High-Fidelity Textures** – HQ tyres, reflections, materials
-<a:animatedboost:1234023770692390922> **Eye Animation** – Realistic animated eyes, just like the film
-<a:animatedboost:1234023770692390922> **Optimized for Performance** – Clean rigging, smooth usage """,
+                <a:animatedboost:1234023770692390922> **4K Ultra-HD Model** – Meticulously detailed for realism
+                <a:animatedboost:1234023770692390922> **Fully Working Lights** – Headlights, brake lights & indicators
+                <a:animatedboost:1234023770692390922> **Movie-Accurate Design** – Faithfully recreated from *Cars 3*
+                <a:animatedboost:1234023770692390922> **High-Fidelity Textures** – HQ tyres, reflections, materials
+                <a:animatedboost:1234023770692390922> **Eye Animation** – Realistic animated eyes, just like the film
+                <a:animatedboost:1234023770692390922> **Optimized for Performance** – Clean rigging, smooth usage """,
         "SEC": """
-### <a:MoneySoaring:1369448460276989982> **PRICE**
+                ### <a:MoneySoaring:1369448460276989982> **PRICE**
 
-*  **\$10 USD** *(Global)* **|  ₹799 INR** *(India)*
+                *  **\$10 USD** *(Global)* **|  ₹799 INR** *(India)*
 
-###  <a:epicvisa:1369685900379951224> **AVAILABLE PAYMENT METHODS**
+                ###  <a:epicvisa:1369685900379951224> **AVAILABLE PAYMENT METHODS**
 
-###  <a:wise:1369686900708872294>  WISE 
+                ###  <a:wise:1369686900708872294>  WISE 
 
-*  <a:epicmail:1369688475670151168> Email: `--------------`
-*  <a:CashMoney:1369690730376728667> Currency: USD / INR
-*  <a:epicnote:1369689849472225325> Reference: *McQueen Mod* """,
-        "THIRD": """
-###  <a:epicupi:1369695243410341949> UPI (For Indian Users)
+                *  <a:epicmail:1369688475670151168> Email: `--------------`
+                *  <a:CashMoney:1369690730376728667> Currency: USD / INR
+                *  <a:epicnote:1369689849472225325> Reference: *McQueen Mod* """,
+                        "THIRD": """
+                ###  <a:epicupi:1369695243410341949> UPI (For Indian Users)
 
-*  <a:epicupi:1369695243410341949> UPI ID: `---------`
-*  <a:epicprofile:1369697072017707045> Name: *------*
-*  <a:epicnote:1369689849472225325> Note: *McQueen Cars 3 Mod*
+                *  <a:epicupi:1369695243410341949> UPI ID: `---------`
+                *  <a:epicprofile:1369697072017707045> Name: *------*
+                *  <a:epicnote:1369689849472225325> Note: *McQueen Cars 3 Mod*
 
-# **Read the Terms & Conditions before proceeding.**
+                # **Read the Terms & Conditions before proceeding.**
 
-🚫 **Terms of Use**
+                🚫 **Terms of Use**
 
-* No refunds after delivery
-* Redistribution or resale is strictly prohibited
-* This is a digital asset; once delivered, it’s final
+                * No refunds after delivery
+                * Redistribution or resale is strictly prohibited
+                * This is a digital asset; once delivered, it’s final """,
+        
+        "FOUR": """ # 🔒 **Leak Protection Notice**
 
-""",
-    "FOUR": """ # 🔒 **Leak Protection Notice**
+                ## All paid files contain a unique **individual identification ID** tied to the buyer. If a file is leaked, we can easily trace it back to the original purchaser.
 
-## All paid files contain a unique **individual identification ID** tied to the buyer. If a file is leaked, we can easily trace it back to the original purchaser.
+                ## **File protection is your responsibility.**
+                ## If a file is leaked from your end, appropriate actions will be taken.
 
-## **File protection is your responsibility.**
-## If a file is leaked from your end, appropriate actions will be taken.
-
-🔐 *Selling mods. Not dreams.*
-💬 Need help? Our team’s always online in **#support** """,
-    "FIVE": """ 
-<a:epicsmile:1234023404986830878>  **PREVIEW**
-https://youtu.be/DGynsgRObZk
-https://youtu.be/_Xn8MrkOa5c
-https://youtu.be/IhS12YicXiQ
-https://media.discordapp.net/attachments/1233831270866227271/1369455997848125540/MC-PRO_PREVIEW_5.png?ex=681c9589&is=681b4409&hm=d887fa828109336e5546b516e330f60a96392309669ab1fc6215f22d4f05211a&=&format=webp&quality=lossless&width=1461&height=822
-https://media.discordapp.net/attachments/1233831270866227271/1369455996509884436/MC-PRO_PREVIEW_2.jpg?ex=681c9588&is=681b4408&hm=374d0362941897fbca00f5fbb13875c017f733da9670105889a161a211a37e1a&=&format=webp&width=1461&height=822
-https://media.discordapp.net/attachments/1233831270866227271/1369456000540737607/MC-PRO_PREVIEW_9.png?ex=681c9589&is=681b4409&hm=e3f0ec87ee91e9c5ca4adbb330d903dd633044858ffee6c7f92411ebfad6cf44&=&format=webp&quality=lossless&width=1461&height=822
-https://media.discordapp.net/attachments/1233831270866227271/1369456000540737607/MC-PRO_PREVIEW_9.png?ex=681c9589&is=681b4409&hm=e3f0ec87ee91e9c5ca4adbb330d903dd633044858ffee6c7f92411ebfad6cf44&=&format=webp&quality=lossless&width=1461&height=822
-https://media.discordapp.net/attachments/1233831270866227271/1369456000540737607/MC-PRO_PREVIEW_9.png?ex=681c9589&is=681b4409&hm=e3f0ec87ee91e9c5ca4adbb330d903dd633044858ffee6c7f92411ebfad6cf44&=&format=webp&quality=lossless&width=1461&height=822
-https://media.discordapp.net/attachments/1233831270866227271/1369455999853006848/MC-PRO_PREVIEW_8.png?ex=681c9589&is=681b4409&hm=a46915146935215e7f1bad546f6a6d6e0d11fe6be64ca0ed84828b3cea006d3e&=&format=webp&quality=lossless&width=1461&height=822
- """
+                🔐 *Selling mods. Not dreams.*
+                💬 Need help? Our team’s always online in **#support** """,
+        "FIVE": """ 
+                <a:epicsmile:1234023404986830878>  **PREVIEW**
+                https://youtu.be/DGynsgRObZk
+                https://youtu.be/_Xn8MrkOa5c
+                https://youtu.be/IhS12YicXiQ
+                https://media.discordapp.net/attachments/1233831270866227271/1369455997848125540/MC-PRO_PREVIEW_5.png?ex=681c9589&is=681b4409&hm=d887fa828109336e5546b516e330f60a96392309669ab1fc6215f22d4f05211a&=&format=webp&quality=lossless&width=1461&height=822
+                https://media.discordapp.net/attachments/1233831270866227271/1369455996509884436/MC-PRO_PREVIEW_2.jpg?ex=681c9588&is=681b4408&hm=374d0362941897fbca00f5fbb13875c017f733da9670105889a161a211a37e1a&=&format=webp&width=1461&height=822
+                https://media.discordapp.net/attachments/1233831270866227271/1369456000540737607/MC-PRO_PREVIEW_9.png?ex=681c9589&is=681b4409&hm=e3f0ec87ee91e9c5ca4adbb330d903dd633044858ffee6c7f92411ebfad6cf44&=&format=webp&quality=lossless&width=1461&height=822
+                https://media.discordapp.net/attachments/1233831270866227271/1369456000540737607/MC-PRO_PREVIEW_9.png?ex=681c9589&is=681b4409&hm=e3f0ec87ee91e9c5ca4adbb330d903dd633044858ffee6c7f92411ebfad6cf44&=&format=webp&quality=lossless&width=1461&height=822
+                https://media.discordapp.net/attachments/1233831270866227271/1369456000540737607/MC-PRO_PREVIEW_9.png?ex=681c9589&is=681b4409&hm=e3f0ec87ee91e9c5ca4adbb330d903dd633044858ffee6c7f92411ebfad6cf44&=&format=webp&quality=lossless&width=1461&height=822
+                https://media.discordapp.net/attachments/1233831270866227271/1369455999853006848/MC-PRO_PREVIEW_8.png?ex=681c9589&is=681b4409&hm=a46915146935215e7f1bad546f6a6d6e0d11fe6be64ca0ed84828b3cea006d3e&=&format=webp&quality=lossless&width=1461&height=822
+                """
+                
 
     },
     
