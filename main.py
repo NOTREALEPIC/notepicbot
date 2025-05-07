ce9544e43132dcc75e32da09700b6fdcc4be89b2
NOTREALEPIC <165712926+NOTREALEPIC@users.noreply.github.com>
2025-05-07 21:42:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -201,7 +201,7 @@ async def fid_autocomplete(interaction: discord.Interaction, current: str):
 
 @tree.command(name="proinfo", description="Get info about paid files",guild=discord.Object(id=1232208366735196283))
 @app_commands.checks.has_role("ROOT")
-@app_commands.describe(code="Enter the file or select from the list.")
+@app_commands.describe(fid="Enter the file or select from the list.")
 @app_commands.autocomplete(fid=fid_autocomplete)
 @commands.cooldown(1, 10, commands.BucketType.user)
 async def proinfo(interaction: discord.Interaction, fid: str):
