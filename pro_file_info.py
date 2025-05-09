f89a3d0d7017f7d1e01da9ac15ba790ea261ec94
NOTREALEPIC <165712926+NOTREALEPIC@users.noreply.github.com>
2025-05-10 00:33:12 +0530

    Update pro_file_info.py

--- pro_file_info.py
+++ pro_file_info.py
@@ -39,20 +39,22 @@ pro_file_info = {
 ## If a file is leaked from your end, appropriate actions will be taken. """,
 
         "FOUR":"""
-                🔐 *Selling mods. Not dreams.*
-                💬 Need help? Our team’s always online in **#support** 
+🔐 *Selling mods. Not dreams.*
+💬 Need help? Our team’s always online in **#support** 
                 
 # <a:epicsmile:1234023404986830878>  **PREVIEW**
 https://youtu.be/DGynsgRObZk
 https://youtu.be/_Xn8MrkOa5c
 https://youtu.be/IhS12YicXiQ
-https://media.discordapp.net/attachments/1233831270866227271/1369455997848125540/MC-PRO_PREVIEW_5.png?ex=681c9589&is=681b4409&hm=d887fa828109336e5546b516e330f60a96392309669ab1fc6215f22d4f05211a&=&format=webp&quality=lossless&width=1461&height=822
-https://media.discordapp.net/attachments/1233831270866227271/1369455996509884436/MC-PRO_PREVIEW_2.jpg?ex=681c9588&is=681b4408&hm=374d0362941897fbca00f5fbb13875c017f733da9670105889a161a211a37e1a&=&format=webp&width=1461&height=822
-https://media.discordapp.net/attachments/1233831270866227271/1369456000540737607/MC-PRO_PREVIEW_9.png?ex=681c9589&is=681b4409&hm=e3f0ec87ee91e9c5ca4adbb330d903dd633044858ffee6c7f92411ebfad6cf44&=&format=webp&quality=lossless&width=1461&height=822
-https://media.discordapp.net/attachments/1233831270866227271/1369456000540737607/MC-PRO_PREVIEW_9.png?ex=681c9589&is=681b4409&hm=e3f0ec87ee91e9c5ca4adbb330d903dd633044858ffee6c7f92411ebfad6cf44&=&format=webp&quality=lossless&width=1461&height=822
-https://media.discordapp.net/attachments/1233831270866227271/1369456000540737607/MC-PRO_PREVIEW_9.png?ex=681c9589&is=681b4409&hm=e3f0ec87ee91e9c5ca4adbb330d903dd633044858ffee6c7f92411ebfad6cf44&=&format=webp&quality=lossless&width=1461&height=822
-https://media.discordapp.net/attachments/1233831270866227271/1369455999853006848/MC-PRO_PREVIEW_8.png?ex=681c9589&is=681b4409&hm=a46915146935215e7f1bad546f6a6d6e0d11fe6be64ca0ed84828b3cea006d3e&=&format=webp&quality=lossless&width=1461&height=822
-                """
+https://youtu.be/9ROYQjyir9o
+https://i.ibb.co/RTWZmrPS/MC-PRO-PREVIEW-1.jpg
+https://i.ibb.co/27J3jf6j/MC-PRO-PREVIEW-3.jpg
+https://i.ibb.co/232SgCf9/MC-PRO-PREVIEW-4.jpg
+https://i.ibb.co/d4FD38Kq/MC-PRO-PREVIEW-5.png
+https://i.ibb.co/5hQkB8g2/MC-PRO-PREVIEW-7.png
+https://i.ibb.co/KjRmtJM8/MC-PRO-PREVIEW-8.png
+https://i.ibb.co/k69s4RrC/MC-PRO-PREVIEW-9.png
+"""
                 
 
     },
